3dab64741a43ef3e1dd383444eb489b55560c151
Ibtehaj Ali <ibtehajali@Ibtehajs-MacBook-Pro.local>
2025-03-29 05:22:23 +0500

    Updated config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 import os
 
-DB_USERNAME = 'ibtehajali'  #
+DB_USERNAME = 'ibtehajali'
 DB_PASSWORD = 'password'
 DB_HOST = 'localhost'
 DB_PORT = '5432'
@@ -9,10 +9,14 @@ DB_NAME = 'mydatabase'
 
 DATABASE_URL = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'secret key')  
 EMBEDDINGS_MODEL_NAME = 'all-MiniLM-L6-v2'  
 
 LOG_FILE = 'logs/pipeline.log'
 LOG_LEVEL = 'INFO'
+
+POD_ID = "tlikdz9ypqfwaj" 
+PORT = "11434"
+
+OLLAMA_BASE_URL = f"https://{POD_ID}-{PORT}.proxy.runpod.net/api/generate"
 
 
--- querrying.py
+++ querrying.py
@@ -1,21 +1,17 @@
 
 from langchain_community.vectorstores import FAISS
-# from langchain_community.embeddings import HuggingFaceEmbeddings
-
 from langchain_huggingface import HuggingFaceEmbeddings
 
-
-
 from models import get_session, Article
-from config import OPENAI_API_KEY, EMBEDDINGS_MODEL_NAME
 from utils import setup_logging
-import openai
 import pickle
 import faiss
 import os
 from joblib import Parallel, delayed
 import requests
 import json
+
+from config import OLLAMA_BASE_URL, EMBEDDINGS_MODEL_NAME
 
 logger = setup_logging()
 
@@ -54,7 +50,6 @@ def create_retriever(vector_store):
 def get_similar_articles(text, retriever, article_dict):
     logger.info(f'Getting similar articles for: {text}')
     try:
-        # docs = retriever.get_relevant_documents(text)
         docs = retriever.invoke(text)
         results = []
         for doc in docs:
@@ -74,46 +69,8 @@ def get_similar_articles(text, retriever, article_dict):
         logger.error(f'Error retrieving similar articles: {e}')
         return []
 
-# def generate_summary(context):
-#     openai.api_key = OPENAI_API_KEY
-#     try:
-#         response = openai.ChatCompletion.create(
-#             model='gpt-4o',  
-#             messages=[
-#                 {"role": "user", "content": f"Summarize the following content:\n\n{context}"}
-#             ],
-#             max_tokens=150,
-#             temperature=0.7
-#         )
-#         summary = response.choices[0].message['content'].strip()
-#         return summary
-#     except Exception as e:
-#         logger.error(f'Error generating summary: {e}')
-#         return f"Error generating summary: {e}"
-
-
 def generate_summary(context):
-    # openai.api_key = OPENAI_API_KEY
     try:
-        # response = openai.ChatCompletion.create(
-        #     model='gpt-4o',  
-        #     messages=[
-        #         {"role": "user", "content": f"Summarize the following content:\n\n{context}"}
-        #     ],
-        #     max_tokens=150,
-        #     temperature=0.7
-        # )
-        # summary = response.choices[0].message['content'].strip()
-        # response = client.chat(model='llama3.2', messages=[
-        #         {
-        #             'role': 'user',
-        #             'content': f'Please summraize this article: \n {context}',
-        #         },
-        #         ])
-        # summary=response.message.content
-
-        url = "https://tlikdz9ypqfwaj-11434.proxy.runpod.net/api/generate"
-
         payload = {
             "model": "llama3.2",
             "prompt": f"Please summarize this article: \n {context}"
@@ -122,11 +79,8 @@ def generate_summary(context):
         headers = {
             "Content-Type": "application/json"
         }
-
-        response = requests.post(url, json=payload, headers=headers)
-
-        # Extracting the text response from streaming output
-        response_lines = response.text.strip().split("\n")  # Each line is a JSON object
+        response = requests.post(OLLAMA_BASE_URL, json=payload, headers=headers)
+        response_lines = response.text.strip().split("\n")
         summary = "".join(json.loads(line)["response"] for line in response_lines if line.strip())
         return summary
     except Exception as e:
